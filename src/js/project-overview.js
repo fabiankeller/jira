574769890ff2afacb8cea53e5541399874f6176a
brookman <rc-p90@bluemail.ch>
2016-05-30 14:42:15 +0200

    removed custom config

--- src/js/project-overview.js
+++ src/js/project-overview.js
@@ -1,5 +1,4 @@
 var projectOverviewGadget = undefined;
-var initProjectOverviewGadget = undefined;
 projectOverviewGadget || (projectOverviewGadget = AJS.Gadget({
     baseUrl: 'http://jira.swisscom.com',
     view: {
@@ -8,21 +7,13 @@ projectOverviewGadget || (projectOverviewGadget = AJS.Gadget({
             var templateReference = this;
             var $ = AJS.$;
 
-            var config = undefined;
-            var defaultConfit = {
+            var config = {
                 project: 'SAM',
                 team: 'Catta',
-                fixVersion: '2016.05 C'
+                version: '2016.05 C'
             };
-
-            initProjectOverviewGadget = function (configParams) {
-                if (configParams) {
-                    config = configParams;
-                } else {
-                    config = defaultConfit;
-                }
-                init();
-            };
+            
+            init();
 
             function appendEpic(id, epic) {
                 $('#overview_epics').append('<div id="' + id + '"><p><b>Epic: ' + epic.key + ' ' + epic.summary + ', SP: ' + epic.sp + ' Time spent: ' + epic.timeSpent + ', Total: ' + epic.aggregateTimeSpent + '</b></p></div>');
@@ -79,7 +70,7 @@ projectOverviewGadget || (projectOverviewGadget = AJS.Gadget({
                 drawChart(epicData);
 
 
-                $.when(jiraUtils.getEpics())
+                $.when(jiraUtils.getEpics(config.project, config.version, config.team))
                     .done(function (epicResponse) {
                         var epics = jiraUtils.getIssuesFromResponse(epicResponse);
 
@@ -87,7 +78,7 @@ projectOverviewGadget || (projectOverviewGadget = AJS.Gadget({
                             var id = 'overview_epic_' + i;
 
                             appendEpic(id, epic);
-                            
+
 //                             $.when(loadStories(epic.key))
 //                                 .done(function (jiraResponse) {
 //                                     getIssues(jiraResponse).forEach(function (story, i) {
