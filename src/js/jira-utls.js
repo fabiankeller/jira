2319c6caa7c667c37a3048d9bc278a71e4dc4659
brookman <rc-p90@bluemail.ch>
2016-05-30 14:33:59 +0200

    moved jira related code to a separate file

---
+++ src/js/jira-utls.js
@@ -0,0 +1,49 @@
+var jiraUtils = undefined;
+(function () {
+    jiraUtils = {
+        jqlQuery: jqlQuery,
+        getEpics: getEpics,
+        getStories: getStories,
+        getSubtasks: getSubtasks,
+        getIssuesFromResponse: getIssuesFromResponse
+    };
+
+    function jqlQuery(queryString) {
+        var url = '/rest/api/2/search?jql=' + encodeURI(queryString);
+        console.log('performing jql query:', url);
+        return $.ajax({
+            url: url,
+            contentType: 'application/json',
+            async: false,
+            dataType: 'json'
+        });
+    }
+
+    function getEpics(project, version, team) {
+        return jqlQuery('project = ' + project + ' AND issuetype = Epic AND fixVersion = "' + version + '" AND Team = ' + team);
+    }
+
+    function getStories(epicId) {
+        return jqlQuery('issueFunction in linkedIssuesOf("\'issue\' = \'' + epicId + '\'", "is Epic of")');
+    }
+
+    function getSubtasks(storyId) {
+        return jqlQuery('issueFunction in subtasksOf("id = ' + storyId + '")');
+    }
+
+    function getIssuesFromResponse(jiraResponse) {
+        var issuesRaw = JSON.parse(jiraResponse.responseText).issues;
+        var issueProcessed = [];
+        issuesRaw.forEach(function (issue, i) {
+            issueProcessed.push({
+                index: i,
+                key: issue.key,
+                summary: issue.fields.summary,
+                sp: issue.fields.customfield_10263,
+                timeSpent: issue.fields.timespent,
+                aggregateTimeSpent: issue.fields.aggregatetimespent,
+                children: []
+            });
+        });
+    }
+})();
--- src/js/project-overview.js
+++ src/js/project-overview.js
@@ -1,5 +1,6 @@
-var timelineGadget;
-timelineGadget || (timelineGadget = AJS.Gadget({
+var projectOverviewGadget = undefined;
+var initProjectOverviewGadget = undefined;
+projectOverviewGadget || (projectOverviewGadget = AJS.Gadget({
     baseUrl: 'http://jira.swisscom.com',
     view: {
         onResizeAdjustHeight: true,
@@ -7,59 +8,39 @@ timelineGadget || (timelineGadget = AJS.Gadget({
             var templateReference = this;
             var $ = AJS.$;
 
-            var config = {
+            var config = undefined;
+            var defaultConfit = {
                 project: 'SAM',
                 team: 'Catta',
                 fixVersion: '2016.05 C'
-            }
+            };
 
-            function makeJqlQuery(queryString) {
-                var url = '/rest/api/2/search?jql=' + encodeURI(queryString);
-                console.log('calling url: ' + url);
-                return $.ajax({
-                    url: url,
-                    contentType: 'application/json',
-                    async: false,
-                    dataType: 'json'
-                });
-            }
-
-            function loadEpics() {
-                return makeJqlQuery('project = ' + config.project + ' AND issuetype = Epic AND fixVersion = "' + config.fixVersion + '" AND Team = ' + config.team);
-            }
-
-            function loadStories(epicId) {
-                return makeJqlQuery('issueFunction in linkedIssuesOf("\'issue\' = \'' + epicId + '\'", "is Epic of")');
-            }
-
-            function loadSubtasks(storyId) {
-                return makeJqlQuery('issueFunction in subtasksOf("id = ' + storyId + '")');
-            }
-
-            function getIssues(jiraResponse) {
-                return JSON.parse(jiraResponse.responseText).issues;
-            }
+            initProjectOverviewGadget = function (configParams) {
+                if (configParams) {
+                    config = configParams;
+                } else {
+                    config = defaultConfit;
+                }
+                init();
+            };
 
             function appendEpic(id, epic) {
-                var sp = epic.fields.customfield_10263;
-                var timeSpent = epic.fields.timespent;
-                var aggregatetimespent = epic.fields.aggregatetimespent;
-                $('#overview_epics').append('<div id="' + id + '"><p><b>Epic: ' + epic.key + ' ' + epic.fields.summary + ', SP: ' + sp + ' Time spent: ' + timeSpent + ', Total: ' + aggregatetimespent + '</b></p></div>');
+                $('#overview_epics').append('<div id="' + id + '"><p><b>Epic: ' + epic.key + ' ' + epic.summary + ', SP: ' + epic.sp + ' Time spent: ' + epic.timeSpent + ', Total: ' + epic.aggregateTimeSpent + '</b></p></div>');
             }
 
-            function appendStory(id, story) {
-                var sp = story.fields.customfield_10263;
-                var timeSpent = story.fields.timespent;
-                var aggregatetimespent = story.fields.aggregatetimespent;
-                $('#' + id).append('<p>---------Story: ' + story.key + ' ' + story.fields.summary + ', SP: ' + sp + ' Time spent: ' + timeSpent + ', Total: ' + aggregatetimespent + '</p>');
-            }
-
-            function appendSubtask(id, subtask) {
-                var sp = subtask.fields.customfield_10263;
-                var timeSpent = subtask.fields.timespent;
-                var aggregatetimespent = subtask.fields.aggregatetimespent;
-                $('#' + id).append('<p>Subtask: ' + subtask.key + ' ' + subtask.fields.summary + ', SP: ' + sp + ' Time spent: ' + timeSpent + ', Total: ' + aggregatetimespent + '</p>');
-            }
+            // function appendStory(id, story) {
+            //     var sp = story.fields.customfield_10263;
+            //     var timeSpent = story.fields.timespent;
+            //     var aggregatetimespent = story.fields.aggregatetimespent;
+            //     $('#' + id).append('<p>---------Story: ' + story.key + ' ' + story.fields.summary + ', SP: ' + sp + ' Time spent: ' + timeSpent + ', Total: ' + aggregatetimespent + '</p>');
+            // }
+            //
+            // function appendSubtask(id, subtask) {
+            //     var sp = subtask.fields.customfield_10263;
+            //     var timeSpent = subtask.fields.timespent;
+            //     var aggregatetimespent = subtask.fields.aggregatetimespent;
+            //     $('#' + id).append('<p>Subtask: ' + subtask.key + ' ' + subtask.fields.summary + ', SP: ' + sp + ' Time spent: ' + timeSpent + ', Total: ' + aggregatetimespent + '</p>');
+            // }
 
             function drawChart(epicData) {
                 google.charts.load('current', {packages: ['corechart', 'bar']});
@@ -94,28 +75,35 @@ timelineGadget || (timelineGadget = AJS.Gadget({
                 ['b', 200, 10]
             ];
 
-            drawChart(epicData);
-            $.when(loadEpics())
-                .done(function (jiraResponse) {
-                    getIssues(jiraResponse).forEach(function (epic, i) {
-                        var id = 'overview_epic_' + i;
-                        appendEpic(id, epic);
-                        $.when(loadStories(epic.key))
-                            .done(function (jiraResponse) {
-                                getIssues(jiraResponse).forEach(function (story, i) {
-                                    appendStory(id, story);
-//													$.when(loadSubtasks(story.key))
-//															.done(function (jiraResponse) {
-//																getIssues(jiraResponse).forEach(function (subtask, i) {
-//																	appendSubtask(id, subtask);
-//																});
-//															});
-                                });
-                            });
+            function init() {
+                drawChart(epicData);
+
+
+                $.when(jiraUtils.getEpics())
+                    .done(function (epicResponse) {
+                        var epics = jiraUtils.getIssuesFromResponse(epicResponse);
+
+                        epics.forEach(function (epic, i) {
+                            var id = 'overview_epic_' + i;
+
+                            appendEpic(id, epic);
+                            
+//                             $.when(loadStories(epic.key))
+//                                 .done(function (jiraResponse) {
+//                                     getIssues(jiraResponse).forEach(function (story, i) {
+//                                         appendStory(id, story);
+// //													$.when(loadSubtasks(story.key))
+// //															.done(function (jiraResponse) {
+// //																getIssues(jiraResponse).forEach(function (subtask, i) {
+// //																	appendSubtask(id, subtask);
+// //																});
+// //															});
+//                                     });
+//                                 });
+                        });
+
                     });
-
-                });
-
+            }
         }
     }
 }));
