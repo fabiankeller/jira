1f5e6197ed97897c13499882f05ae722f4e99291
brookman <rc-p90@bluemail.ch>
2016-06-27 16:24:51 +0200

    debugging

--- src/js/jira-utils.js
+++ src/js/jira-utils.js
@@ -59,8 +59,7 @@
     function resizeGadget(gadgetId) {
         console.log('resize gadget', gadgetId);
         console.log('iframe', window.parent.AJS.$("#" + gadgetId + " iframe"));
-        window.parent.AJS.$("#" + gadgetId + " iframe").css("height", 500);
-        console.log('height', window.parent.AJS.$("#" + gadgetId + " iframe").css("height"));
+        window.parent.AJS.$("#gadget-50378 iframe").css("height", 500);
         window.parent.AJS.$.each(window.parent.AG.DashboardManager.activeLayout.getGadgets(), function (index, gadget) {
             console.log('resize', gadget);
             gadget.resize();
